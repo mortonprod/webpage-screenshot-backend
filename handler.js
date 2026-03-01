4fab026305d763e02d337b977462801165ef3ffd
Alexander Morton <mortonprod@gmail.com>
2026-03-01 16:24:08 +0000

    Claude update (#1)

--- handler.js
+++ handler.js
@@ -1,103 +1,97 @@
-// https://codissimo.sinumo.tech/2019/12/27/serverless-puppeteer-with-aws-lambda-layers-and-node-js/
-const chromeLambda = require("chrome-aws-lambda");
-const S3Client = require("aws-sdk/clients/s3");
-const s3 = new S3Client({ region: process.env.S3_REGION });
-const {v4 : uuidv4} = require('uuid')
+const chromium = require("@sparticuz/chromium-min");
+const puppeteer = require("puppeteer-core");
+const { S3Client, PutObjectCommand, GetObjectCommand } = require("@aws-sdk/client-s3");
+const { getSignedUrl } = require("@aws-sdk/s3-request-presigner");
+const { v4: uuidv4 } = require("uuid");
 
-exports.screenshot = async (event, context, callback) => {
+const s3 = new S3Client({ region: process.env.S3_REGION });
+
+exports.screenshot = async (event) => {
   console.log(JSON.stringify(event));
+
   if (!event.queryStringParameters || !event.queryStringParameters["url"]) {
-    return {
-      statusCode: 400
-    };
+    return { statusCode: 400 };
   }
   if (!event.headers || !event.headers["User-Agent"]) {
-    return {
-      statusCode: 400
-    };
+    return { statusCode: 400 };
   }
   if (!event.headers || !event.headers["origin"]) {
-    return {
-      statusCode: 400
-    };
+    return { statusCode: 400 };
   }
-  // Must check CORS here otherwise the server will still run even if not from the correct origin.
   if (!event.headers["origin"].includes("https://pagemelt.alexandermorton.co.uk")) {
-    return {
-      statusCode: 401
-    };
+    return { statusCode: 401 };
   }
+
   const url = decodeURIComponent(event.queryStringParameters["url"]);
   const userAgent = event.headers["User-Agent"];
   const id = uuidv4();
+
   try {
     console.log(`IP: ${event.requestContext.identity.sourceIp}, URL: ${event.queryStringParameters["url"]}`);
     const vw = Number(event.queryStringParameters["vw"]) || Number(process.env.WIDTH);
     let vh = Number(event.queryStringParameters["vh"]) || Number(process.env.HEIGHT);
     vh = Number(vh) + Number(process.env.SCROLL_HEIGHT);
-    const buffer = await getBuffer(url,vw,vh,userAgent);
-    let Key = getBucketName(url,id,vw,vh);
-    const result = await s3.upload({
+
+    const buffer = await getBuffer(url, vw, vh, userAgent);
+    const Key = getBucketKey(url, id, vw, vh);
+
+    await s3.send(
+      new PutObjectCommand({
         Bucket: process.env.S3_BUCKET,
         Key,
         Body: buffer,
-        ContentType: "image/jpg",
-        CacheControl: 'max-age=86400'
-      }).promise();
-    const signedUrl = s3.getSignedUrl('getObject', {
-        Bucket: process.env.S3_BUCKET,
-        Key,
-        Expires: 30
-    });
+        ContentType: "image/jpeg",
+        CacheControl: "max-age=86400",
+      })
+    );
+
+    const signedUrl = await getSignedUrl(
+      s3,
+      new GetObjectCommand({ Bucket: process.env.S3_BUCKET, Key }),
+      { expiresIn: 30 }
+    );
 
     return {
       statusCode: 200,
       headers: {
-        'Access-Control-Allow-Origin': 'https://pagemelt.alexandermorton.co.uk'
+        "Access-Control-Allow-Origin": "https://pagemelt.alexandermorton.co.uk",
       },
-      body: JSON.stringify({
-          url: signedUrl
-      })
+      body: JSON.stringify({ url: signedUrl }),
     };
   } catch (e) {
     console.error(JSON.stringify(e));
     return {
       statusCode: 500,
       headers: {
-        'Access-Control-Allow-Origin': 'https://pagemelt.alexandermorton.co.uk'
-      }
+        "Access-Control-Allow-Origin": "https://pagemelt.alexandermorton.co.uk",
+      },
     };
   }
 };
 
-function getBucketName(url, id, width, height) {
-  /// Remove protocols
-  url = url.replace(/https:\/\//, '').replace(/http:\/\//, '');
-  /// Replace . with _
-  url = url.replace(/[\.]/g,'_');
-  // Replace / with __
-  url = url.replace(/[\/]/g,'__');
-  /// Split width height and id using ____
+function getBucketKey(url, id, width, height) {
+  url = url.replace(/https?:\/\//, "");
+  url = url.replace(/\./g, "_");
+  url = url.replace(/\//g, "__");
   return `${url}___${width}___${height}___${id}.jpg`;
 }
 
-async function getBuffer(url,width, height,userAgent) {
+async function getBuffer(url, width, height, userAgent) {
+  const browser = await puppeteer.launch({
+    args: chromium.args,
+    executablePath: await chromium.executablePath(),
+    defaultViewport: {
+      width: Number(width),
+      height: Number(height),
+    },
+  });
   try {
-    const browser = await chromeLambda.puppeteer.launch({
-      args: chromeLambda.args,
-      executablePath: await chromeLambda.executablePath,
-      defaultViewport: {
-        width: Number(width),
-        height: Number(height)
-      } 
-    });
     const page = await browser.newPage();
     await page.setUserAgent(userAgent);
     await page.goto(url);
-    const buffer = await page.screenshot({quality: 30, type: "jpeg"})
+    const buffer = await page.screenshot({ quality: 30, type: "jpeg" });
     return buffer;
-  } catch (e) {
-    console.error(e);
-    throw e;
+  } finally {
+    await browser.close();
   }
 }
